146a66b1b72d7979286aedc82d151c2915e22739
shahruz <hello@shahr.uz>
2021-04-19 16:15:01 -0700

    test vercel builder

--- src/app/components/create/CreatePortfolio.tsx
+++ src/app/components/create/CreatePortfolio.tsx
@@ -59,7 +59,7 @@ const CreatePortfolio = () => {
             : null
       };
       const { built } = await fetch(
-        'https://factory-sites.gimmix.org/api/build',
+        'https://factory-sites.vercel.app/api/build',
         {
           method: 'POST',
           body: JSON.stringify({ config }),
